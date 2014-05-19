6035092989fb46d141134d380ebd50cef4452b53
Ben McGinnes <ben@adversary.org>
2014-05-20 00:12:21 +1000

    Added shell script to run tests.  Will not work on Windows.

---
+++ run-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./foad.py
+./foad.py list_options
+pydoc3 foad.py
+./foad.py unittest
+./foad.py unittest Bob the Builder
+./foad.py unittest carnal
+./foad.py unittest bond
+./foad.py unittest die
+./foad.py unittest right
+./foad.py unittest title
+./foad.py unittest cunt
+./foad.py unittest foaas
+./foad.py unittest snag
