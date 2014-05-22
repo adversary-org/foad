2d5253609447ccc7357442491904920f677aa924
Ben McGinnes <ben@adversary.org>
2014-05-23 02:28:59 +1000

    Wrestling with the horror of this thing.
    Don't particularly want to do it.

--- oldfoad.py
+++ oldfoad.py
@@ -9,7 +9,6 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.5.5.2a
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # License:  GNU Public License version 3 (GPLv3)
@@ -24,43 +23,7 @@
 # Versions up to 0.4.x developed with Python 2.7.x.  Conversion to
 # Python 3 made from version 0.4.2.
 #
-# Options and notes:
-#
-# Based on Fuck Off As A Service (FOAAS)
-# https://foaas.herokuapp.com/
-#
-# Includes the same options and many more beyond those.
-#
-# As the name suggests, most of the output involves the many uses of
-# the word "fuck", but some options do not use it.  Running the
-# unittest will display the output for all options and show which
-# options do not use the word "fuck".
-#
-# The unittest will display the correct output for almost everything.
-#
-# The "sherlock" option if it is run with "Sherlock" or "Sherlock
-# Holmes" as the target will only produce one result normally, but the
-# unittest won't pick that up properly.  The "random" option, however,
-# will give the correct output if it happens to hit that option with
-# that target.
-#
-# The acronyms options also does not behave like the main options,
-# instead it uses the the target parameter to look up different
-# definitions.
-#
-# Using the acronym option without a target displays an introduction.
-# Using the acronym option with a target not specifically addressed
-# displays the list of target parameters which can be used with that
-# option.  Using any of the listed target parameters will display the
-# relevant definition.
-#
-# There are three options on Latin.  The "priapus" options include
-# translation in the "priapus_trans1" and "priapus_trans2" options
-# (priapus_trans2 no target contains the translation for priapus no
-# target and priapus_trans1 contains the best translation for priapus
-# with a target, the other variants are potentially useful).  The
-# "omnia" and "vvv" options include translations in comments in the
-# source code.
+# A wrapper for foad.py
 #
 # Old Usage:  oldfoad.py donut foo
 #             oldfoad.py outside "FirstName LastName"
@@ -87,10 +50,58 @@ __copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
 __copyrightu__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
 __title__ = "FOAD: Fucked Off Adversarial Degenerates (Fuck Off And Die)"
 __license__ = "GNU Public License version 3 (GPLv3)"
-__version__ = "0.5.5.2a"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
-about = """Wrapper for foad.py to use it the old way."
+about = "Wrapper for foad.py to use it the old way."
 
+from foad import __version__
+import os
+import subprocess
 import sys
+
+foad = "./foad.py"
+p = os.popen
+s = os.system
+#p = subprocess.Popen
+#s = subprocess.call
+#s = subprocess.check_output
+sa = sys.argv
+l = len(sa)
+
+if l == 1:
+    print("""
+    %s
+
+    You must specify a type of fuck to give.  Target optional.
+
+    To use run:  oldfoad.py <option> [<target>]
+    To test all output run:  oldfoad.py unittest [<target>]
+    For random output run:  oldfoad.py random [<target>]
+
+    To see the defined options run:  oldfoad.py list_options
+
+    You should switch to the new version as more options will become
+    available.  For more details run:  foad -h
+
+    https://github.com/adversary-org/foad
+
+    %s
+    Bitcoin:  %s
+    """ % (__title__, __copyright__, __bitcoin__))
+    wtf = ""
+    target = ""
+    #cmd = ""
+elif l == 2:
+    wtf = sa[1].lower()
+    target = ""
+    cmd = "%s -f %s" % (foad, wtf)
+    s(cmd)
+elif l >= 3:
+    wtf = sa[1].lower()
+    t = []
+    for i in range(l - 2):
+        t.append(str(sa[i + 2]))
+    target = " ".join(t)
+    cmd = "%s -f %s -n %s" % (foad, wtf, target)
+    s(cmd)
