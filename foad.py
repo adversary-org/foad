e4d0d611888167ec188cc2c0e265de8f276990ea
Ben McGinnes <ben@adversary.org>
2014-09-08 08:43:54 +1000

    Version 0.7.0:
    Script passes all tests in foad.py -f unittest with new string
    handling format.  Prepared to release as version 0.7.
    
    Also, preliminary testing indicates the script works with PyPy3
    (2.3.1).  Not enough testing has been done yet to claim that as
    guaranteed, though.

--- foad.py
+++ foad.py
@@ -9,7 +9,7 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.7.0.0-beta
+# Version:  0.7.0.0
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # License:  GNU Public License version 3 (GPLv3)
@@ -119,7 +119,7 @@ __copyrighth__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
 __title__ = "FOAD: Fucked Off Adversarial Degenerates (Fuck Off And Die)"
 __stitle__ = "FOAD"
 __license__ = "GNU Public License version 3 (GPLv3)"
-__version__ = "0.7.0.0-beta"
+__version__ = "0.7.0.0"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
