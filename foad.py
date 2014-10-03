3d480f99147ef14e9df2b5264aff16432cd72b1e
Ben McGinnes <ben@adversary.org>
2014-10-03 17:42:10 +1000

    Added caniuse option (may require some additional versions).

--- foad.py
+++ foad.py
@@ -9,7 +9,7 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.7.1.3
+# Version:  0.7.1.4
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # License:  GNU Public License version 3 (GPLv3)
@@ -121,7 +121,7 @@ __title__ = "FOAD: Fucked Off Adversarial Degenerates (Fuck Off And Die)"
 __stitle__ = "FOAD"
 __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
-__version__ = "0.7.1.3"
+__version__ = "0.7.1.4"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
@@ -377,6 +377,13 @@ class fuck:
             msg = "Big bad motherfucker."
         elif lt == 0 and ls > 0:
             msg = "{0} is a big bad motherfucker.".format(sender)
+        print(msg)
+
+    def caniuse(self):
+        if lt == 0 and ls == 0 and le == 0:
+            msg = "Can I Use Notes: This option requires the tool/object be specified with --extra and is a two-parter rolled into one command.  Currently requires --name, --extra and --sender."
+        elif lt > 0 and ls > 0 and le > 0:
+            msg = """{0}, it's {1} here, can I use {2}?  ...  Can you use {3}?  Fuck no, {4}!  You cannot fucking use {5}!""".format(sender, target, extra, extra, target, extra)
         print(msg)
 
     def cango(self):
