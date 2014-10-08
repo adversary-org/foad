e6420945541bcb7ac87761a58e61b292e3e9e23d
Ben McGinnes <ben@adversary.org>
2014-10-09 05:44:59 +1100

    Added nugget option.

--- foad.py
+++ foad.py
@@ -9,7 +9,7 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.7.1.7
+# Version:  0.7.1.8
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # License:  GNU Public License version 3 (GPLv3)
@@ -121,7 +121,7 @@ __title__ = "FOAD: Fucked Off Adversarial Degenerates (Fuck Off And Die)"
 __stitle__ = "FOAD"
 __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
-__version__ = "0.7.1.7"
+__version__ = "0.7.1.8"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
@@ -1085,6 +1085,19 @@ class fuck:
             msg = "Not safe for work?  Of course it's not fucking safe for work!"
         else:
             msg = "Not safe for work?  {0}, of course it's not fucking safe for work!".format(target)
+        print(msg)
+
+    def nugget(self):
+        if lt == 0 and ls == 0 and le == 0:
+            msg = "Well, aren't you a shining example of a rancid fuck-nugget."
+        if lt > 0 and ls == 0 and le == 0:
+            msg = "Well {0}, aren't you a shining example of a rancid fuck-nugget.".format(target)
+        if lt > 0 and ls > 0 and le == 0:
+            msg = "Well {0}, aren't you a shining example of a rancid fuck-nugget.  -- {1}".format(target, sender)
+        if lt > 0 and ls == 0 and le > 0:
+            msg = "Well {0}, aren't you a shining example of a rancid fuck-nugget.  {1}".format(target, extra)
+        else:
+            msg = "Well {0}, aren't you a shining example of a rancid fuck-nugget.  {1}  -- {2}".format(target, extra, sender)
         print(msg)
 
     def oath1(self):
