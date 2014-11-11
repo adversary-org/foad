7323c9a77d42c5c906ee3d2c42fcf029fdd43eb3
Ben McGinnes <ben@adversary.org>
2014-11-12 07:28:23 +1100

    Added the first of what should become many Amber options.

--- foad.py
+++ foad.py
@@ -9,7 +9,7 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.7.3.7
+# Version:  0.7.3.8
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # License:  GNU Public License version 3 (GPLv3)
@@ -121,7 +121,7 @@ __title__ = "FOAD: Fucked Off Adversarial Degenerates (Fuck Off And Die)"
 __stitle__ = "FOAD"
 __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
-__version__ = "0.7.3.7"
+__version__ = "0.7.3.8"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
@@ -389,6 +389,20 @@ class fuck:
             msg = "{0}, that was fucking amazing!".format(target)
         else:
             msg = "{0}, that was fucking amazing!".format(target)
+        return msg
+
+# All the amber options are taken or adapted from _The Chronicles of
+# Amber_ by Roger Zelazny.  More than one is simply inevitable.
+
+    def amber1(self):
+        if lt == 0 and lR == 0:
+            msg = "Of all my relations I like sex the best and you the least."
+        elif lt > 0 and lR == 0:
+            msg = "Of all my relations I like sex the best and {0} the least.".format(target)
+        elif lt > 0 and lR > 0:
+            msg = "{0}, of all my relations I like sex the best and {1} the least.".format(relay, target)
+        else:
+            msg = "Of all my relations I like sex the best and {0} the least.".format(target)
         return msg
 
     def apple(self):
