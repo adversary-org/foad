ba2596ec8e91f99e29a673ad181d5387aad6336d
Ben McGinnes <ben@adversary.org>
2014-12-19 07:52:10 +1100

    Updated you option.
    While "fuck you, fuck your X and fuck Z" will be most common,
    hardcoding "your" in that sequence does reduce some possibilities.

--- foad.py
+++ foad.py
@@ -2053,13 +2053,13 @@ class fuck:
         elif lt > 0 and le > 0 and ls == 0 and lR == 0:
             msg = "Fuck you {0} and fuck {1}!".format(target, extra)
         elif lt == 0 and le > 0 and ls > 0 and lR == 0:
-            msg = "Fuck you, fuck your {0} and fuck {1}!".format(sender, extra)
+            msg = "Fuck you, fuck {0} and fuck {1}!".format(sender, extra)
         elif lt == 0 and le > 0 and ls > 0 and lR > 0:
-            msg = "{0}, tell them: fuck you, fuck your {1} and fuck {2}!".format(relay, sender, extra)
+            msg = "{0}, tell them: fuck you, fuck {1} and fuck {2}!".format(relay, sender, extra)
         elif lt > 0 and le > 0 and ls > 0 and lR == 0:
-            msg = "Fuck you {0}, fuck your {1} and fuck {2}!".format(target, sender, extra)
+            msg = "Fuck you {0}, fuck {1} and fuck {2}!".format(target, sender, extra)
         elif lt > 0 and le > 0 and ls > 0 and lR > 0:
-            msg = "{0}, tell {1}: fuck you {2}, fuck your {3} and fuck {4}!".format(relay, target, target, sender, extra)
+            msg = "{0}, tell {1}: fuck you {2}, fuck {3} and fuck {4}!".format(relay, target, target, sender, extra)
         return msg
 
 fucked = fuck()
