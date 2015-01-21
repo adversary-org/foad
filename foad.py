773217de19a6c86044df3ab3f432223dc4bbe0a1
Ben McGinnes <ben@adversary.org>
2015-01-21 18:58:21 +1100

    Version 0.7.7.3
    Added code comment so I don't remove something which appears
    superfluous, but really isn't (the la/lx bits).
    
    Also, in the previous commit the options modified were tism and
    compleat, not tism and foad.

--- foad.py
+++ foad.py
@@ -9,7 +9,7 @@
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.7.7.2
+# Version:  0.7.7.3
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # Licenses:  GNU Public License version 3 (GPLv3)
@@ -125,7 +125,7 @@ __stitle__ = "FOAD"
 __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
 __license3__ = "New BSD (3 clause) type"
-__version__ = "0.7.7.2"
+__version__ = "0.7.7.3"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
@@ -215,6 +215,9 @@ parser.add_argument("-p", "--prepend", help="Additional comment to prepend befor
 # parser.add_argument("-o", "--output", help="Writes output to the specified file instead of stdout.", action="store", required=False)
 parser.add_argument("-O", "--options", help="Lists the explicit variations (the same as: -f list_options), will accept any argument to activate.", action="store", required=False)
 parser.add_argument("-V", "--version", help="Print the version number.", action="store", required=False)
+
+# This, in conjunction with lx above is what allows the old style
+# usage to continue working:
 
 if len(sys.argv) > lx:
     la = len(sys.argv)
