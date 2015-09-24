17256dda4d3b72adc5f52eccb773c9696dd780e5
Ben McGinnes <ben@adversary.org>
2015-09-24 23:38:35 +1000

    Fixing foad/froad
    * removed a possibility of producing a traceback error.

--- foad.py
+++ foad.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.8.0.3
+# Version:  0.8.0.4
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # Licenses:  GNU Public License version 3 (GPLv3)
@@ -141,7 +141,7 @@ __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
 __license3__ = "New BSD (3 clause) type"
 __license4__ = "Apache 2.0"
-__version__ = "0.8.0.3"
+__version__ = "0.8.0.4"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 
@@ -934,13 +934,12 @@ class fuck:
         return msg
 
     def foad(self):
-        if "foad" in sa[0]:
-            try:
-                exec("fuck().foad1()")
-            except:
-                exec("foad.fuck().foad1()")
-        else:
-            print("Help guide for foad.py (pydoc3 foad).")
+        msg = """
+Try froad as the type of fuck to give instead.
+
+Alternatively run the help guide for foad.py (pydoc3 foad).
+"""
+        return msg
 
     def froad(self):
         if lt == 0 and le == 0:
