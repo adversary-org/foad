f62a826ab41e29416cf4b64cab2f8febda1382b8
Ben McGinnes <ben@adversary.org>
2017-06-26 06:04:10 +1000

    Give
    * Updated option: give

--- foad.py
+++ foad.py
@@ -14,7 +14,7 @@ from __future__ import division
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.8.1.10
+# Version:  0.8.1.11
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # Licenses:  GNU Public License version 3 (GPLv3)
@@ -139,7 +139,7 @@ __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
 __license3__ = "New BSD (3 clause) type"
 __license4__ = "Apache 2.0"
-__version__ = "0.8.1.10"
+__version__ = "0.8.1.11"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 __openpgp_fpr__ = "DB4724E6FA4286C92B4E55C4321E4E2373590E5D"
@@ -1151,10 +1151,16 @@ Alternatively run the help guide for foad.py (pydoc3 foad).
         return msg
 
     def give(self):
-        if lt == 0:
+        if lt == 0 and le == 0:
             msg = "I don't give a fuck."
-        else:
+        elif lt == 0 and le > 0:
+            msg = "I really don't give a fuck."
+        elif lt > 0 and le == 0:
+            msg = "{0}, I don't give a fuck.".format(target)
+        elif lt > 0 and le > 0:
             msg = "{0}, I really don't give a fuck.".format(target)
+        else:
+            msg = "Give a fuck?"  # should never appear
         return msg
 
     def gived(self):
