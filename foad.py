52eeb8fb26775ac9d216601033514b80c0c47b9a
Ben McGinnes <ben@adversary.org>
2017-11-29 22:18:13 +1100

    Off you fuck.
    * The first of what will inevitably be a series of additions either
      inspired by or due to Charlotte.

--- foad.py
+++ foad.py
@@ -14,7 +14,7 @@ from __future__ import division
 #
 # https://github.com/adversary-org/foad
 #
-# Version:  0.8.1.16
+# Version:  0.8.1.17
 #
 # BTC:  1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz
 # Licenses:  GNU Public License version 3 (GPLv3)
@@ -139,7 +139,7 @@ __license1__ = "GNU General Public License version 3 (GPLv3)"
 __license2__ = "Do What The Fuck You Want To, But It's Not My Fault Public License version 1 (WTFNMFPLv1)"
 __license3__ = "New BSD (3 clause) type"
 __license4__ = "Apache 2.0"
-__version__ = "0.8.1.16"
+__version__ = "0.8.1.17"
 __bitcoin__ = "1NpzDJg2pXjSqCL3XHTcyYaehiBN3kG3Lz"
 __openpgp__ = "0x321E4E2373590E5D"
 __openpgp_fpr__ = "DB4724E6FA4286C92B4E55C4321E4E2373590E5D"
@@ -2351,6 +2351,18 @@ Alternatively run the help guide for foad.py (pydoc3 foad).
         else:
             msg = "{0}, you are a complete and utter fucktard!".format(target)
         return msg
+
+    def talky(self):
+        if lt == 0 and le == 0:
+            msg = "I'm not felling very talky today. Off you fuck."
+        elif lt == 0 and le > 0:
+            msg = "I'm not felling very talky today. Off you fuck!"
+        elif lt > 0 and le == 0:
+            msg = "I'm not felling very talky today, {0}. Off you fuck.".format(target)
+        elif lt > 0 and le > 0:
+            msg = "I'm not felling very talky today, {0}. Off you fuck!".format(target)
+        else:
+            msg = "I'm not felling very talky today. Off you fuck!"
     
     def tfwo(self):
         if lt == 0:
